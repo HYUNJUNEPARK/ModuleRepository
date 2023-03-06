ebea6d8182921452ff1c3c722eb32f442e625f54
HYUNJUNE <june2ac4dev@gmail.com>
2023-03-06 22:32:19 +0900

    Extract DatabaseManager

--- ModuleSimulator/app/src/main/java/com/dev/modulesimulator/MainActivity.kt
+++ ModuleSimulator/app/src/main/java/com/dev/modulesimulator/MainActivity.kt
@@ -4,13 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.LifecycleOwner
 import com.dev.modulesimulator.databinding.ActivityMainBinding
 import com.manager.pref.SharedPreferencesManager
-import com.manger.database.model.Item
 import com.manger.database.viewmodel.ItemViewModel
-import java.text.SimpleDateFormat
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -25,23 +21,23 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.mainActivity = this@MainActivity
 
-        //sharedPreferencesManager = SharedPreferencesManager.getInstance(applicationContext)
+        //TODO SharedPreferencesManager
+//        sharedPreferencesManager = SharedPreferencesManager.getInstance(applicationContext)
 
-        itemViewModel = ItemViewModel(application)
-        itemViewModel.itemList.observe(this) { itemList ->
-            Log.d(TAG, "itemList.observe: $itemList")
-        }
+        //TODO DatabaseManager
+//        itemViewModel = ItemViewModel(application)
+//        itemViewModel.itemList.observe(this) { itemList ->
+//            Log.d(TAG, "itemList.observe: $itemList")
+//        }
+
     }
 
     fun test1() {
         Log.d(TAG, "test1")
-        val item = Item(id = System.currentTimeMillis(), content = "test1")
-        itemViewModel.add(item)
     }
 
     fun test2() {
         Log.d(TAG, "test2")
-        itemViewModel.deleteAllItems()
     }
 
     fun test3() {
